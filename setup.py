cbb04936f29c2ee92bed7533b541ddff1a4b189a
Laurence Cook <lwc24@cam.ac.uk>
2017-07-18 17:58:52 +0100

    Fixed installation setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-setup(name='hypersonicvehicle',
+setup(name='hypersonicsimulation',
         version='0.0.1',
-        url='https://www-edc.eng.cam.ac.uk/aerotools/hypersonicvehicle',
-        download_url='https://github.com/lwcook/hypersonic-vehicle/archive/0.0.1.tar.gz',
+        url='https://www-edc.eng.cam.ac.uk/aerotools/hypersonicsimulation',
+        download_url='https://github.com/lwcook/hypersonic-simulation/archive/0.0.1.tar.gz',
         author='Laurence W. Cook',
         author_email='lwc24@cam.ac.uk',
         install_requires=['numpy >= 1.12.1'],
         license='MIT',
-        packages=['horsetailmatching'],
+        packages=['hypersonicsimulation'],
         test_suite='nose.collector',
         tests_require=['nose'],
         include_package_data=True,
